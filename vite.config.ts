95ef0e26afe55ff9f83f00d7bd2b6250ebda3f83
yunisharra <yunisharra30@gmail.com>
2026-02-15 03:19:15 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
+    // Ini sangat penting agar process.env.API_KEY tidak bikin crash di browser
     'process.env': {}
   },
   build: {
